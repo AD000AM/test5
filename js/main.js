b1c2f5ea4fae7d7127a2eb2f31d9646718ebc964
AD00AM <adam.moh.omar@gmail.com>
2024-01-21 20:57:48 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,65 +1,83 @@
+let todayName = document.getElementById("today_date_day_name")
+let todayNumber = document.getElementById("today_date_day_number")
+let todayMonth = document.getElementById("today_date_month")
+let todayLocation = document.getElementById("today_location")
+let todayTemp = document.getElementById("today_temp")
+let todayConditionImg = document.getElementById("today_condition_img")
+let todayConditionText = document.getElementById("today_condition text")
+let humidity = document.getElementById("humidity")
+let wind = document. getElementById("wind")
+let windDirection = document.getElementById("wind_direction")
+
+//next date
+let nextDay = document.getElementsByClassName("next_day_name")
+let nextMaxTemp = document.getElementsByClassName("next_max_temp")
+let nextMinTemp = document.getElementsByClassName("next_min_temp")
+let nextConditionImg = document.getElementsByClassName("next_condition_img")
+let nextConditionText = document.getElementsByClassName("next_condition_text")
+//search input
+let searchInput = document.getElementById("search")
+let date = new Date()
 
 
-function redirectToNewAccount() {
-  document.getElementById("loginPage").style.display = "none";
-  document.getElementById("newAccountPage").style.display = "block";
+//fetch api
+async function getWeatherdata(cityName)
+{
+  let WetherResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=42caee5b67bd473ca7c145129241701&q=${cityName}&days=3`)
+  let weatherData = await WetherResponse.json()
+  return weatherData
+}
+getWeatherdata()
+
+//display today data 
+function displayTodayData(data)
+{
+  let TodayDate = new Date()
+  todayName.innerHTML = TodayDate.toLocaleDateString("en-US", {weekday:"long"} )
+  todayNumber.innerHTML = TodayDate.getDate()
+  todayMonth.innerHTML = TodayDate.toLocaleDateString("en-US",{month:"long"})
+  todayLocation.innerHTML = data.location.name
+  todayTemp.innerHTML = data.current.temp_c
+  todayConditionImg.setAttribute("src",data.current.condition.icon)
+  todayConditionText.innerHTML = data.current.condition.text
+  humidity.innerHTML = data.current.humidity+ "%"
+  wind.innerHTML = data.current.wind_kph+ " km/h"
+  windDirection.innerHTML = data.current.wind_dir
+}
+// display 
+function displayNextData(data)
+{
+ let forecastData = data.forecast.forecastday
+ for(let i = 0; i<2 ; i++)
+ {
+  let nextDate = new Date(forecastData[i+1].date)
+  nextDay[i].innerHTML = nextDate.toLocaleDateString("en-US",{weekday:"long"})
+  nextMaxTemp[i].innerHTML = forecastData[i+1].day.maxtemp_c
+  nextMinTemp[i].innerHTML = forecastData[i+1].day.mintemp_c
+  nextConditionImg[i].setAttribute("src", forecastData[i+1].day.condition.icon)
+  nextConditionText[i].innerHTML = forecastData[i+1].day.condition.text
+
+ }
 }
 
-function redirectToLoginPage() {
-  document.getElementById("newAccountPage").style.display = "none";
-  document.getElementById("loginPage").style.display = "block";
+// start 
+
+async function startApp(city="cairo")
+{
+  let wheatherData = await getWeatherdata(city)
+if(!wheatherData.error){
+
+  displayTodayData(wheatherData)
+  displayNextData(wheatherData)
+}
+  
 }
 
+startApp()
 
-function validateEmail(email) {
-  var emailRegex = /\S+@\S+\.\S+/;
-  return emailRegex.test(email);
-}
-
-function login(email, password) {
-  var email = document.getElementById("loginEmailInput").value;
-  var password = document.getElementById("loginPasswordInput").value;
-  let users = JSON.parse(localStorage.getItem("users"))
-  let result = users.find((el) => {
-    return el.email2 == email && el.password2 == password
-  })
-
-  if (result != -1) {
-    var username = email.substring(0, email.indexOf("@"));
-    document.getElementById("welcomeMessage").textContent = "Welcome, " + username + "!";
-    document.getElementById("loginPage").style.display = "none";
-    document.getElementById("welcomePage").style.display = "block";
-  } else {
-    alert("Invalid credentials. Please try again.");
-  }
-}
+searchInput.addEventListener("input", function(){
+  startApp(searchInput.value)
+})
 
 
 
-
-
-var UserDataArray = []
-function redirectToLoginPage() {
-  var name = document.getElementById("nameInput").value;
-  var email2 = document.getElementById("emailInput").value;
-  var password2 = document.getElementById("passwordInput").value;
-
-  var userdata = {
-    name: name,
-    email2: email2,
-    password2: password2,
-  };
-  UserDataArray.push(userdata)
-  localStorage.setItem("users", JSON.stringify(UserDataArray))
-
-  if (name.trim() === "") {
-    alert("Please enter your name.");
-  } else if (!validateEmail(email2)) {
-    alert("Please enter a valid email address.");
-  } else if (password2.length < 8) {
-    alert("Please enter a password with at least 8 characters.");
-  } else {
-    alert("Sign up successful!");
-    window.location.href = "file:///C:/Users/adam/OneDrive/Desktop/week10/index.html";
-  }
-}
